a0f4339e2444e5da004ce9d460efd2a289defaeb
Andrii Prystai <cras123@gmail.com>
2021-02-12 16:24:45 +0100

    Starting first react proj

---
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Messsages</a>
+    </div>
+  );
+};
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
+export default App;
