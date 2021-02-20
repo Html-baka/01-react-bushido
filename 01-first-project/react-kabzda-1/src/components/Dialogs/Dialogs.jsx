1a72c3caef9cccb897ae2e138c9308f590e4feae
unknown <cras123@gmail.com>
2021-02-20 19:48:54 +0100

    added dialogs option, will be updated soon

--- 01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ 01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,26 @@
-import s from './Dialogs.module.css'
-const Dialogs = (props) => {
-   return (
-      <div className="s.content">
-         Dialogs
-      </div>
-   )
-}
+import s from "./Dialogs.module.css";
 
-export default Dialogs
+const Dialogs = (props) => {
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>Hohlojin</div>
+
+        <div className={s.dialog}>Ded Klop</div>
+
+        <div className={s.dialog}>Petuh</div>
+
+        <div className={s.dialog}>Klop 1</div>
+
+        <div className={s.dialog}>Klop 2</div>
+      </div>
+      <div className={s.messages}>
+         <div className={s.message}>Hi</div>
+         <div className={s.message}>How is your react?</div>
+         <div className={s.message}>heeeey?</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dialogs;
