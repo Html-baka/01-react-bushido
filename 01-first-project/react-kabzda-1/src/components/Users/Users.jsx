9b394cc74a85d2787830366d6cf620ad42da7ee3
unknown <cras123@gmail.com>
2021-03-08 20:17:39 +0100

    added Users page + data structuring

---
+++ 01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import s from "./users.module.css";
+
+let Users = (props) => {
+debugger
+   if (props.users.length === 0) {
+      props.setUsers([
+         
+      ])
+   }
+  return (
+    <div>
+      {
+         props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img src={u.photoUrl} className={s.userPhoto}/>
+            </div>
+            <div>
+               { u.followed 
+                  ? <button onClick={()=> { props.unfollow(u.id)}}>Unfollow</button> 
+                  : <button onClick={()=> { props.follow(u.id)}}>Follow</button> }
+            </div>
+          </span>
+          <span>
+             <div>{u.fullName}</div>
+             <div>{u.status}</div>
+          </span>
+          <span>
+            <div>{u.location.city}</div>
+            <div>{u.location.country}</div>
+          </span>
+        </div>
+         ))
+      }
+    </div>
+  );
+};
+
+export default Users;
