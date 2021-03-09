762510265e316b9ac2d2498ea36e6aea62260acb
unknown <cras123@gmail.com>
2021-03-09 17:55:37 +0100

    Users bug fix

--- 01-first-project/react-kabzda-1/src/redux/users-reducer.js
+++ 01-first-project/react-kabzda-1/src/redux/users-reducer.js
@@ -2,34 +2,7 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS'
 let initialState = {
-  users: [{  id: 1,
-    photoUrl: 'https://u.livelib.ru/reader/Simak_ss3497/r/lelxg9ii/lelxg9ii-r.jpg',
-    followed: false,
-    fullName: "KiT",
-    status: "Meow..",
-    location: { city: "Kiev", country: "Ukraine" },
-  },
-  { id: 2,
-    photoUrl: 'https://i.pinimg.com/originals/7f/2a/50/7f2a500aee5a59ea8722fcaf43d8ba09.png',
-    followed: true,
-    fullName: "yoba",
-    status: "trollong ur mom",
-    location: { city: "zalupivka", country: "Ukraine" },
-  },
-  { id: 3,
-    photoUrl: 'https://3.bp.blogspot.com/-x0lDfNSLUZw/XBp4fTKUtYI/AAAAAAAAJLg/VTy1rY8n5P0jqOu2nbGqSkZjtq2W8p8tgCLcBGAs/s1600/deeb0efba601645f32215e72e166e732cc082591_hq.jpg',
-    followed: false,
-    fullName: "korgi",
-    status: "kiiik",
-    location: { city: "moscow", country: "russia" },
-  },
-  { id: 4,
-    photoUrl: 'https://www.meme-arsenal.com/memes/acd328c37f112fcb0ee6bbfc79973d10.jpg',
-    followed: true,
-    fullName: "lepr",
-    status: "ooooo moya oborona",
-    location: { city: "Minsk", country: "Belarus" }
-  }]
+  users: []
 };
 const usersReducer = (state = initialState, action) => {
   
@@ -55,7 +28,7 @@ const usersReducer = (state = initialState, action) => {
       })
     }
     case SET_USERS:
-      return { ...state, users: [ ...state.users, action.users ]}
+      return { ...state, users: [ ...state.users, ...action.users ]}
 
       default:
       return state;
