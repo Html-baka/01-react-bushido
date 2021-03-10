ca13182f4d8924ef49538f83ba21944551c902cf
unknown <cras123@gmail.com>
2021-03-10 18:33:57 +0100

    implemented class components

--- 01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ 01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import s from "./users.module.css";
 import * as axios from "axios";
 import user_undefined from "../../assets/images/user_undefined.png"
-let Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-       axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-         debugger
-         props.setUsers(response.data.items)
-       });
-    }
-  }
-  return (
-    <div>
-        <button onClick={getUsers}>Get Users</button>
+
+
+class Users extends React.Component {
+    constructor(props) {
+      super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+          debugger
+          this.props.setUsers(response.data.items)
+        });
+   }
+    
+    render() {
+      return <div>
       {
-        props.users.map((u) => (
+        this.props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src={ u.photos.small != null ? u.photos.small : user_undefined} className={s.userPhoto}/>
+              <img src={ u.photos.small != null ? u .photos.small : user_undefined} className={s.userPhoto}/>
             </div>
             <div>
                { u.followed 
-                  ? <button onClick={()=> { props.unfollow(u.id)}}>Unfollow</button> 
-                  : <button onClick={()=> { props.follow(u.id)}}>Follow</button> }
+                  ? <button onClick={()=> { this.props.unfollow(u.id)}}>Unfollow</button> 
+                  : <button onClick={()=> { this.props.follow(u.id)}}>Follow</button> }
             </div>
           </span>
           <span>
@@ -39,7 +40,8 @@ let Users = (props) => {
          ))
       }
     </div>
-  );
-};
+    }
+  }
+
 
 export default Users;
